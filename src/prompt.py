c69819df344a0508cc6b36d94d9992d01c2683dd
jgtv <jgtvidal@gmail.com>
2025-06-10 15:56:46 +0100

    reasoning

--- src/prompt.py
+++ src/prompt.py
@@ -2,7 +2,7 @@ from population import Organism
 
 
 class Prompgenerator:
-    def __init__(self, systemprompt: str):
+    def __init__(self, systemprompt: str, reason: bool = False):
         self.systemprompt = systemprompt
 
     def generate_prompt(self, organism: Organism) -> str:
@@ -10,6 +10,8 @@ class Prompgenerator:
 {self.systemprompt}
 
 {self._previous_solution(organism)}
+
+{self.format()}
 """
         return otpt
 
@@ -24,3 +26,11 @@ It achieved a fitness of {organism.fitness}.
 You should propose a new solution that is a SMALL ITERATIVE IMPROVEMENT on this one.
 """
         return otpt
+    
+    def format(self) -> str:
+        if not self.reason:
+            return "Your answer should just be the new solution in code, with no additional text or formatting."
+        else:
+            return r"You should output any reasoning you need, and then the new solution in code, with no additional text or formatting. Use /n---/n to separate the reasoning from the solution."
+        
+
--- src/specification.py
+++ src/specification.py
@@ -2,10 +2,21 @@
 from typing import Callable, Optional
 from pydantic import BaseModel
 
+
+
+class Hyperparameters(BaseModel):
+    exploration_rate: float = 0.4
+    elitism_rate: float = 0.1
+    max_steps: int = 100
+    target_fitness: Optional[float] = None
+    reason: bool = False
+
+
 class ProblemSpecification(BaseModel):
     name: str
     systemprompt: str
-    evlator: Callable[[str], float]
+    evaluator: Callable[[str], float]
     sample_solution: str
-    target_fitness: Optional[float] = None
-    max_steps: Optional[int] = None
+    hyperparameters: Hyperparameters
+
+
